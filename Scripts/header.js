71df269d33a768e432ccdf5aabec44acacf2c72e
xJekk <jekkproducer@gmail.com>
2023-05-19 16:38:55 +0200

    index.html

--- Scripts/header.js
+++ Scripts/header.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 let height = window.scrollY;
 
@@ -9,15 +8,3 @@ window.addEventListener("scroll", () => {
     }
 })
 
-=======
-
-let height = window.scrollY;
-
-window.addEventListener("scroll", () => {
-    height = window.scrollY;
-    if (height > 160) {
-        
-    }
-})
-
->>>>>>> 12ab5022b22e6a675443b913774412ece7ea029e
